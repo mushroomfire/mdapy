f37934476103387aebeb695012bc11311a1eceae
mushroomfire <934313174@qq.com>
2022-10-11 16:53:33 +0800

    updated

--- __init__.py
+++ __init__.py
@@ -10,17 +10,18 @@ from .src.centro_symmetry_parameter import CentroSymmetryParameter
 from .plot.pltset import pltset, cm2inch
 
 
-def init(arch="cpu", debug=False, device_memory_GB=2.0):
+def init(arch="cpu", debug=False, device_memory_GB=2.0, kernel_profiler=False):
     """
     arch : str, "cpu" or "gpu", default is "cpu".
     debuge : bool, default is False.
     device_memory_GB : float, memory for GPU only, default is 2 GB.
+    kernel_profiler : bool, default is False.
     """
     import taichi as ti
 
     if arch == "cpu":
-        ti.init(arch=ti.cpu, debug=debug)
+        ti.init(arch=ti.cpu, debug=debug,kernel_profiler=kernel_profiler)
     elif arch == "gpu":
-        ti.init(arch=ti.gpu, device_memory_GB=device_memory_GB, debug=debug)
+        ti.init(arch=ti.gpu, device_memory_GB=device_memory_GB, debug=debug, kernel_profiler=kernel_profiler)
     else:
         raise ValueError("Unrecognized arch, please choose in ['cpu', 'gpu'].")

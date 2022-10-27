6a3120b47a4a0773571723605e301116c16d9cdc
mushroomfire <934313174@qq.com>
2022-10-27 20:33:49 +0800

    updated

--- __init__.py
+++ __init__.py
@@ -13,24 +13,36 @@ from .plot.pltset import pltset, cm2inch
 import taichi.profiler as profiler
 
 
-def init(arch="cpu", debug=False, device_memory_GB=2.0, kernel_profiler=False):
+def init(
+    arch="cpu",
+    offline_cache=False,
+    debug=False,
+    device_memory_GB=2.0,
+    kernel_profiler=False,
+):
     """
     arch : str, "cpu" or "gpu", default is "cpu".
     debuge : bool, default is False.
+    offline_cache : bool, defults is False.
     device_memory_GB : float, memory for GPU only, default is 2 GB.
     kernel_profiler : bool, default is False.
     """
     import taichi as ti
 
     if arch == "cpu":
-        ti.init(arch=ti.cpu, debug=debug, kernel_profiler=kernel_profiler)
+        ti.init(
+            arch=ti.cpu,
+            offline_cache=offline_cache,
+            debug=debug,
+            kernel_profiler=kernel_profiler,
+        )
     elif arch == "gpu":
         ti.init(
             arch=ti.gpu,
+            offline_cache=offline_cache,
             device_memory_GB=device_memory_GB,
             debug=debug,
             kernel_profiler=kernel_profiler,
         )
-        ti.init(arch=ti.cpu, debug=debug, kernel_profiler=kernel_profiler)
     else:
         raise ValueError("Unrecognized arch, please choose in ['cpu', 'gpu'].")

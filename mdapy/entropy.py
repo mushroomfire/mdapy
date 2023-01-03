0030dbfd4a26c64338bb07eb8f1ad8806aa59a65
mushroomfire <934313174@qq.com>
2023-01-03 14:17:59 +0800

    updated entropy doc

--- mdapy/entropy.py
+++ mdapy/entropy.py
@@ -7,6 +7,93 @@ import numpy as np
 
 @ti.data_oriented
 class AtomicEntropy:
+    """This class is used to calculate the entropy fingerprint, which is useful to distinguish
+    between ordered and disordered environments, including liquid and solid-like environments,
+    or glassy and crystalline-like environments. The potential application could identificate grain boundaries
+    or a solid cluster emerging from the melt. One of the advantages of this parameter is that no a priori
+    information of structure is required.
+
+    This parameter for atom :math:`i` is computed using the following formula:
+
+    .. math:: s_S^i=-2\\pi\\rho k_B \\int\\limits_0^{r_m} \\left [ g(r) \\ln g(r) - g(r) + 1 \\right ] r^2 dr,
+
+    where :math:`r` is a distance, :math:`g(r)` is the radial distribution function,
+    and :math:`\\rho` is the density of the system.
+    The :math:`g(r)` computed for each atom :math:`i` can be noisy and therefore it can be smoothed using:
+
+    .. math:: g_m^i(r) = \\frac{1}{4 \\pi \\rho r^2} \\sum\\limits_{j} \\frac{1}{\\sqrt{2 \\pi \\sigma^2}} e^{-(r-r_{ij})^2/(2\\sigma^2)},
+
+    where the sum over :math:`j` goes through the neighbors of atom :math:`i` and :math:`\\sigma` is
+    a parameter to control the smoothing. The average of the parameter over the neighbors of atom :math:`i`
+    is calculated according to:
+
+    .. math:: \\left< s_S^i \\right>  = \\frac{\\sum_j s_S^j + s_S^i}{N + 1},
+
+    where the sum over :math:`j` goes over the neighbors of atom :math:`i` and :math:`N` is the number of neighbors.
+    The average version always provides a sharper distinction between order and disorder environments.
+
+    .. note:: If you use this module in publication, you should also cite the original paper.
+      `Entropy based fingerprint for local crystalline order <https://doi.org/10.1063/1.4998408>`_
+
+    .. note:: This class uses the `same algorithm with LAMMPS <https://docs.lammps.org/compute_entropy_atom.html>`_.
+
+    .. tip:: Suggestions for FCC, the :math:`rc = 1.4a` and :math:`average\_rc = 0.9a` and
+      for BCC, the :math:`rc = 1.8a` and :math:`average\_rc = 1.2a`, where the :math:`a`
+      is the lattice constant.
+
+    Args:
+        vol (float): system volume.
+
+        verlet_list (np.ndarray): (:math:`N_p * max\_neigh`) verlet_list[i, j] means j atom is a neighbor of i atom if j > -1.
+
+        distance_list (np.ndarray): (:math:`N_p * max\_neigh`) distance_list[i, j] means distance between i and j atom.
+
+        rc (float, optional): cutoff distance. Defaults to 5.0.
+
+        sigma (float, optional): smoothing parameter. Defaults to 0.2.
+
+        use_local_density (bool, optional): whether use local atomic volume. Defaults to False.
+
+        compute_average (bool, optional): whether compute the average version. Defaults to False.
+
+        average_rc (float, optional): cutoff distance for averaging operation, if not given, it is equal to rc. This parameter should be lower than rc.
+
+    Examples:
+        >>> import mdapy as mp
+
+        >>> mp.init()
+
+        >>> import numpy as np
+
+        >>> FCC = mp.LatticeMaker(3.615, 'FCC', 10, 10, 10) # Create a FCC structure
+
+        >>> FCC.compute() # Get atom positions
+
+        >>> neigh = mp.Neighbor(FCC.pos, FCC.box,
+                                3.615*1.4, max_neigh=50) # Initialize Neighbor class.
+
+        >>> neigh.compute() # Calculate particle neighbor information.
+
+        >>> vol = np.product(FCC.box[:, 1] - FCC.box[:, 0]) # Calculate system volume.
+
+        >>> Entropy = mp.AtomicEntropy(
+                vol,
+                neigh.verlet_list,
+                neigh.distance_list,
+                neigh.rc,
+                sigma=0.2,
+                use_local_density=False,
+                compute_average=True,
+                average_rc=3.615*0.9,
+            ) # Initilize the entropy class.
+
+        >>> Entropy.compute() # Calculate the atomic entropy.
+
+        >>> Entropy.entropy # Check atomic entropy.
+
+        >>> Entropy.entropy_average # Check averaged atomic entropy.
+    """
+
     def __init__(
         self,
         vol,
@@ -18,6 +105,7 @@ class AtomicEntropy:
         compute_average=False,
         average_rc=None,
     ):
+
         self.vol = vol
         self.verlet_list = verlet_list
         self.distance_list = distance_list
@@ -104,7 +192,7 @@ class AtomicEntropy:
             entropy_average[i] /= neigh_num
 
     def compute(self):
-
+        """Do the real entropy calculation."""
         self.N = self.distance_list.shape[0]
         self.entropy = np.zeros(self.N)
         self.global_density = self.N / self.vol
@@ -142,10 +230,11 @@ if __name__ == "__main__":
     end = time()
     print(f"Build {FCC.pos.shape[0]} atoms FCC time: {end-start} s.")
     start = time()
-    neigh = Neighbor(FCC.pos, FCC.box, 5.0, max_neigh=60)
+    neigh = Neighbor(FCC.pos, FCC.box, lattice_constant * 1.4, max_neigh=60)
     neigh.compute()
     end = time()
     print(f"Build neighbor time: {end-start} s.")
+    print(neigh.neighbor_number.max())
 
     start = time()
     vol = np.product(FCC.box[:, 1] - FCC.box[:, 0])
@@ -157,7 +246,7 @@ if __name__ == "__main__":
         sigma=0.2,
         use_local_density=False,
         compute_average=True,
-        average_rc=4.0,
+        average_rc=lattice_constant * 0.9,
     )
     Entropy.compute()
     entropy = Entropy.entropy

9fcfeb4f7c3f787eaa83ff829cc31e1ed1ef3498
mushroomfire <934313174@qq.com>
2023-01-05 20:09:09 +0800

    updated WCP doc

--- mdapy/warren_cowley_parameter.py
+++ mdapy/warren_cowley_parameter.py
@@ -9,17 +9,52 @@ from .plotset import pltset, cm2inch
 
 @ti.data_oriented
 class WarrenCowleyParameter:
-    """
-    Calculate the average WCP parameter for system, to characteristic the short range order in alloy.
-    input:
-    verlet_list : ndarray
-    neighbor : ndarray
-    type_list : ndarray
-    return :
-    WCP : ndarray
+    """This class is used to calculate the Warren Cowley parameter (WCP), which is useful to
+    analyze the short-range order (SRO) in the 1st-nearest neighbor shell in alloy system and is given by:
+
+    .. math:: WCP_{mn} = 1 - Z_{mn}/(X_n Z_{m}),
+
+    where :math:`Z_{mn}` is the number of :math:`n`-type atoms around :math:`m`-type atoms,
+    :math:`Z_m` is the total number of atoms around :math:`m`-type atoms, and :math:`X_n` is
+    the atomic concentration of :math:`n`-type atoms in the alloy.
+
+    .. note:: If you use this class in publication, you should also cite the original paper:
+      `X‐Ray Measurement of Order in Single Crystals of Cu3Au <https://doi.org/10.1063/1.1699415>`_.
+
+    Args:
+        verlet_list (np.ndarray): (:math:`N_p, max\_neigh`) verlet_list[i, j] means j atom is a neighbor of i atom if j > -1.
+        neighbor_number (np.ndarray): (:math:`N_p`) neighbor atoms number.
+        type_list (np.ndarray): (:math:`N_p`) atom type.
+
+    Outputs:
+        - **WCP** (np.ndarray) - (:math:`N_{type}, N_{type}`) WCP for all pair elements
+
+    Examples:
+        >>> import mdapy as mp
+
+        >>> mp.init()
+
+        >>> system = mp.System("./example/CoCuFeNiPd-4M.dump") # Read a alloy DUMP file.
+
+        >>> neigh = mp.Neighbor(system.pos, system.box, 3.0, max_neigh=30) # Initialize Neighbor class.
+
+        >>> neigh.compute() # Calculate particle neighbor information.
+
+        >>> wcp = mp.WarrenCowleyParameter(
+            neigh.verlet_list, neigh.neighbor_number, system.data["type"].values
+            ) # Initilize the WCP class.
+
+        >>> wcp.compute() # Calculate the WCP.
+
+        >>> wcp.WCP # Check the WCP matrix.
+
+        This should get the same results with that in paper: `Simultaneously enhancing the ultimate strength and ductility of high-entropy alloys via short-range ordering <https://doi.org/10.1038/s41467-021-25264-5>`_.
+
+        >>> wcp.plot(["Co", "Cu", "Fe", "Ni", "Pd"]) # Plot the results.
     """
 
     def __init__(self, verlet_list, neighbor_number, type_list):
+
         self.verlet_list = verlet_list
         self.neighbor_number = neighbor_number
         self.type_list = type_list - 1
@@ -52,6 +87,7 @@ class WarrenCowleyParameter:
             Alpha_n[i] /= N
 
     def compute(self):
+        """Do the real WCP calculation."""
         Ntype = len(np.unique(self.type_list))
         Zmn = np.zeros((Ntype, Ntype), dtype=np.int32)
         Zm = np.zeros(Ntype, dtype=np.int32)
@@ -68,6 +104,17 @@ class WarrenCowleyParameter:
         self.WCP = 1 - Zmn / (Alpha_n * Zm)
 
     def plot(self, elements_list=None, vmin=-2, vmax=1, cmap="GnBu"):
+        """Plot the WCP matrix.
+
+        Args:
+            elements_list (list, optional): elements list, such as ['Al', 'Ni', 'Co'].
+            vmin (int, optional): vmin for colorbar. Defaults to -2.
+            vmax (int, optional): vmax for colorbar. Defaults to 1.
+            cmap (str, optional): cmap name. Defaults to "GnBu".
+
+        Returns:
+            tuple: (fig, ax) matplotlib figure and axis class.
+        """
         fig, ax = plt.subplots(figsize=(cm2inch(8), cm2inch(8)), dpi=150)
         plt.subplots_adjust(bottom=0.1, top=0.9, left=0.15, right=0.82)
         h = ax.imshow(self.WCP[::-1], vmin=vmin, vmax=vmax, cmap=cmap)
@@ -122,6 +169,6 @@ if __name__ == "__main__":
     wcp.compute()
     end = time()
     print(f"Cal WCP time: {end-start} s.")
-    # print("WCP matrix is:")
-    # print(wcp.WCP)
+    print("WCP matrix is:")
+    print(wcp.WCP)
     wcp.plot(["Co", "Cu", "Fe", "Ni", "Pd"])

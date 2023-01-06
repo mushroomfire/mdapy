95dd56b8a328bc658d52739a626e0923121499d3
mushroomfire <934313174@qq.com>
2023-01-06 16:34:44 +0800

    updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except Exception:
 
 setup(
     name="mdapy",
-    version="0.7.4",
+    version="0.7.5",
     author="mushroomfire aka HerrWu",
     author_email="yongchao_wu@bit.edu.cn",
     description=description,

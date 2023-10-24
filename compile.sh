fb9658251c6741ac2b9bfcd4465bda564f6d1b11
mushroomfire <934313174@qq.com>
2023-10-24 21:48:51 +0800

    updated

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 # To compile the C module for different python versions.
 #!/usr/bin/env bash
 
-version="0.9.4"
+version="0.9.5"
 
 if [ "$(uname)" == "Darwin" ]; then
     source ~/anaconda3/etc/profile.d/conda.sh

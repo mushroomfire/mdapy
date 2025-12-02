541c189e44789e9f97c949a3e798828fff589b83
mushroomfire <934313174@qq.com>
2025-12-02 11:20:15 +0200

    update

--- doc/build_doc.sh
+++ doc/build_doc.sh
@@ -4,7 +4,7 @@ sphinx-apidoc -o source ../src/mdapy/
 rm source/modules.rst
 sed -i '/^Module contents/,$d' source/mdapy.rst
 sed -i '1,5d' source/mdapy.rst
-sed -i '1i Index\n====\n' source/mdapy.rst
+sed -i '1i Index\n======\n' source/mdapy.rst
 echo "✅ Cleaned up source/mdapy.rst"
 
 make clean && make html

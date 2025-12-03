b57cf1ef2ddfd784b2c3fc177cead551442fa27f
herrwu <934313174@qq.com>
2025-12-03 23:49:19 +0200

    update test for adf

--- tests/test_angular_distribution_function.py
+++ tests/test_angular_distribution_function.py
@@ -11,6 +11,9 @@ def test_adf():
             "O-H-H": [0, 2.0, 0, 2.0],
             "O-O-H": [0, 2.0, 0, 2.0],
             "H-H-H": [0, 2.0, 0, 2.0],
+            "H-O-O": [0, 2.0, 0, 2.0],
+            "O-O-O": [0, 2.0, 0, 2.0],
+            "H-O-H": [0, 2.0, 0, 2.0],
         },
         40,
     )
@@ -33,3 +36,9 @@ def test_adf():
             assert np.allclose(histogram[:, column], adf.bond_angle_distribution[1])
         elif name == "H-H-H":
             assert np.allclose(histogram[:, column], adf.bond_angle_distribution[2])
+        elif name == "O-H-O":
+            assert np.allclose(histogram[:, column], adf.bond_angle_distribution[3])
+        elif name == "O-O-O":
+            assert np.allclose(histogram[:, column], adf.bond_angle_distribution[4])
+        elif name == "O-H-H":
+            assert np.allclose(histogram[:, column], adf.bond_angle_distribution[5])
